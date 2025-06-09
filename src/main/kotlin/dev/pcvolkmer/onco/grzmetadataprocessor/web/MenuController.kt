09832317113c5f5d4f488606db94fdaef67474be
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-06-09 18:46:12 +0200

    feat: show lab data profile count in menu

--- src/main/kotlin/dev/pcvolkmer/onco/grzmetadataprocessor/web/MenuController.kt
+++ src/main/kotlin/dev/pcvolkmer/onco/grzmetadataprocessor/web/MenuController.kt
@@ -2,6 +2,7 @@ package dev.pcvolkmer.onco.grzmetadataprocessor.web
 
 import dev.pcvolkmer.onco.grzmetadataprocessor.data.CaseRepository
 import dev.pcvolkmer.onco.grzmetadataprocessor.data.FileRepository
+import dev.pcvolkmer.onco.grzmetadataprocessor.data.LabDataProfileRepository
 import dev.pcvolkmer.onco.grzmetadataprocessor.data.LabDataRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping
 class MenuController(
     private val caseRepository: CaseRepository,
     private val labDataRepository: LabDataRepository,
-    private val fileRepository: FileRepository
+    private val fileRepository: FileRepository,
+    private val labDataProfileRepository: LabDataProfileRepository
 ) {
 
     @GetMapping("/cases/menu")
@@ -19,6 +21,7 @@ class MenuController(
         model.addAttribute("cases", caseRepository.findAll().count())
         model.addAttribute("unusedLabData", labDataRepository.countLabDataByDonorIdIsNull())
         model.addAttribute("unusedFiles", fileRepository.findByLabDataIdIsNull().size)
+        model.addAttribute("labDataProfiles", labDataProfileRepository.count())
         return "casemenu"
     }
 
